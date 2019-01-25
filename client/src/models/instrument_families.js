2ad7a53161caa32d8a13222350285f99e5766b53
Ernest Fraiman <upapaus@gmail.com>
2019-01-25 13:48:36 +0000

    replaced prottotypes with classes functions

--- client/src/models/instrument_families.js
+++ client/src/models/instrument_families.js
@@ -8,20 +8,37 @@ class InstrumentFamilies {
   constructor(data) {
     this.data = data;
   }
+
+  bindEvents(){
+    PubSub.publish('InstrumentFamilies:data-ready', this.data);
+
+    PubSub.subscribe('SelectView:change', (evt) => {
+      const selectedIndex = evt.detail;
+      this.publishFamilyDetail(selectedIndex);
+    });
+  }
+
+  publishFamilyDetail(selectedIndex) {
+    const selectedFamily = this.data[selectedIndex];
+    PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  }
 }
 
-InstrumentFamilies.prototype.bindEvents = function () {
-  PubSub.publish('InstrumentFamilies:data-ready', this.data);
+// InstrumentFamilies.prototype.bindEvents = function () {
+//   PubSub.publish('InstrumentFamilies:data-ready', this.data);
+//
+//   PubSub.subscribe('SelectView:change', (evt) => {
+//     const selectedIndex = evt.detail;
+//     this.publishFamilyDetail(selectedIndex);
+//   });
+// };
 
-  PubSub.subscribe('SelectView:change', (evt) => {
-    const selectedIndex = evt.detail;
-    this.publishFamilyDetail(selectedIndex);
-  });
-};
+// InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
+//   const selectedFamily = this.data[selectedIndex];
+//   PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+// };
 
-InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
-  const selectedFamily = this.data[selectedIndex];
-  PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
-};
+// Method
+
 
 module.exports = InstrumentFamilies;
